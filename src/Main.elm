1e1a0fc6b7e6190e23ead386e3e1d324ca47fc48
Khalid <k.dahir243@hotmail.com>
2022-03-11 19:12:03 -0600

    Init commit

---
+++ src/Main.elm
@@ -0,0 +1,5 @@
+module Main exposing (main)
+
+import Html exposing (text)
+
+main = text "Blog page for the development of major projects such as the Fabled Engine, Pilot Launcher, and Fabled Editor. (Working progress I Will be using Elm most likely for designing the blog page."
