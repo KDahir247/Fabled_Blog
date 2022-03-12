df1e1a3b76ee1db68bfb26261d8fd663bc864361
Khalid <k.dahir243@hotmail.com>
2022-03-11 20:00:31 -0600

    Created place holder while developing blog site

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,24 @@
-module Main exposing (main)
+module Main exposing (..)
 
-import Html exposing (text)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Font as Font
 
-main = text "Calling from Elm!!"
+main =
+    Element.layout
+        [ Background.color (rgba 0 0 0 1)
+        , Font.color (rgba 1 1 1 1)
+        , Font.italic
+        , Font.size 32
+        , Font.family
+            [ Font.external
+                { url = "https://fonts.googleapis.com/css?family=EB+Garamond"
+                , name = "EB Garamond"
+                }
+            , Font.sansSerif
+            ]
+        ]
+    <|
+        el
+            [ centerX, centerY ]
+            (text "Blog is under construction check back soon!")
